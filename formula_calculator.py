7d8d82d0ffeb9412cc8bae3722559b66ab2e92a8
Axel Sorenson <axelpsorenson@gmail.com>
2023-09-04 04:21:34 -0700

    Finished a rough card power level calculator. Also added power levels to sigils in the database. I'm likely not gonna continue updating this as it's pretty rough.

--- formula_calculator.py
+++ formula_calculator.py
@@ -1,14 +1,78 @@
 from card import Card
 from helpers import db_connect
+
+BONE_VALUE = (3.0 / 2.5)
+ENERGY_VALUE = 1.0
+BLOOD_VALUE = {
+    0: 0,
+    1: 3,
+    2: 8,
+    3: 14,
+    4: 21
+}
+GEM_VALUE = {
+    0: 0,
+    1: 2,
+    2: 5,
+    3: 8
+}
+
+def get_stats_value(card, stat):
+    value = 0
+    if card.stats[stat] is not None:
+        value = card.stats[stat]
+    return value
+
+def get_cost_value(card, cost):
+    value = 0
+    if card.costs[cost] is not None:
+        value = card.costs[cost]
+    return value
+
 @db_connect
 def calculate_balance(cursor):
     # SELECT and fetch to retrieve all cards from table
     cursor.execute('SELECT * FROM cards')
     rows = cursor.fetchall()
 
-    # Generating all cards
+    # Checking balance of all cards
     for row in rows:
+        stat_points_spent = 0
         card = Card(*row)
+
+        print(card.name + ': ')
+        # Get value of card - ie how many points can be spent for attack, health, sigils, etc.
+        blood = get_cost_value(card, 'blood')
+        bone = get_cost_value(card, 'bone')
+        energy = get_cost_value(card, 'energy')
+        orange = get_cost_value(card, 'orange')
+        green = get_cost_value(card, 'green')
+        blue = get_cost_value(card, 'blue')
+        stat_point_value = BLOOD_VALUE[blood] +\
+                           BONE_VALUE * bone +\
+                           ENERGY_VALUE * energy +\
+                           GEM_VALUE[orange + green + blue]
+        if stat_point_value == 0:
+            stat_point_value = 1
+
+        # Get amount of points spent on card - ie the more powerful the card is, the more points were obviously spent on it.
+        power = get_stats_value(card, 'power')
+        health = get_stats_value(card, 'health')
+
+        cursor.execute(f'SELECT * FROM card_sigils WHERE card_filename = ?', (card.filename,))
+        sigils = cursor.fetchall()
+        for sigil in sigils:
+            stat_points_spent += sigil[2]
+
+        stat_points_spent = (power * 2) + health
+        if card.types['rarity'] == 'rare' or '_Talking' in card.filename:
+            stat_points_spent += 1
+
+
+        print('Card Value: '+str(stat_point_value))
+        print('Card Power: '+str(float(stat_points_spent)))
+        print('Point Surplus/Shortage: '+str(int(stat_point_value - stat_points_spent))+'\n')
+
 
 if __name__ == '__main__':
     calculate_balance()
